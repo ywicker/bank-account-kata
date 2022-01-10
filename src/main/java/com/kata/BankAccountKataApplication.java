2e21bc26489d766f9642ec602f78a4357ec96724
Yoann Wicker <yoann.wicker@gmail.com>
2022-01-10 14:01:54 +0100

    init

---
+++ src/main/java/com/kata/BankAccountKataApplication.java
@@ -0,0 +1,13 @@
+package com.kata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAccountKataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kata/BankAccountKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
