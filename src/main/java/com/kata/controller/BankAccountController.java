937c0979979c35211199d9809cb8f2e4395838dd
ywick <yoann.wicker@gmail.com>
2022-01-10 18:02:23 +0100

    US2 sans TU

--- src/main/java/com/kata/controller/BankAccountController.java
+++ src/main/java/com/kata/controller/BankAccountController.java
@@ -42,4 +42,22 @@ public class BankAccountController {
 		accountService.updateBalance(transaction.getCustomerId(), transaction.getAmount());
 		return accountService.getBalance(transaction.getCustomerId());
 	}
+
+	/**
+	 * Permet de retirer de l'argent
+	 * curl -X POST localhost:8080/withdrawal -H 'Content-type:application/json' -d '{"customerId":1, "amount":-2}'
+	 * 
+	 * @param transaction contenant l'id du client et le montant
+	 * @return le nouveau solde
+	 * @throws Exception si le montant est positif
+	 */
+	@PostMapping("/withdrawal")
+	public int withdrawal(@RequestBody Transaction transaction) throws Exception {
+		if(transaction.getAmount() > 0) { 
+			throw new Exception("La transaction n'a pas pu etre realisee car le montant est positif");
+		}
+
+		accountService.updateBalance(transaction.getCustomerId(), transaction.getAmount());
+		return accountService.getBalance(transaction.getCustomerId());
+	}
 }
